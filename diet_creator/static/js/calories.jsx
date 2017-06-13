4d1ee100e4a163d9416a7671486f2ed34e839fa2
Katsiaryna Valenchyts <k.valenchyts@codemaster.by>
2017-06-13 22:26:29 +0300

    Fixed bugs from Mr. Healthmen.

--- diet_creator/static/js/calories.jsx
+++ diet_creator/static/js/calories.jsx
@@ -9,16 +9,16 @@ var Calories = React.createClass({
 			<div>
 				<div className="calories-wrap">
                     <div className="title">Variance of calorie calculation values per day, Kcal</div>
-                    <div className="item">Minimum <span className="bolder min">2506</span></div>
-                    <div className="item">Average <span className="bolder aver">2794</span><span className="bold proc"> Scatter: ±25.18% (704 Kcal)</span></div>
-                    <div className="item">Maximum <span className="bolder max">3498</span></div>
+                    <div className="item">Minimum <span className="bolder min"></span></div>
+                    <div className="item">Average <span className="bolder aver"></span><span className="bold proc"></span></div>
+                    <div className="item">Maximum <span className="bolder max"></span></div>
                 </div>
 				<div className="bju">
                     <div className="title">The recommended daily amount</div>
-                    <div className="item">Proteins, g <span className="bolder belc">79.52</span></div>
-                    <div className="item">Including animal proteins, g <span className="bolder jivotbelc">43.73</span></div>
-                    <div className="item">Fats, g <span className="bolder jir">92.70</span></div>
-                    <div className="item">Carbohydrates, g <span className="bolder uglev">409.16</span></div>
+                    <div className="item">Proteins, g <span className="bolder belc"></span></div>
+                    <div className="item">Including animal proteins, g <span className="bolder jivotbelc"></span></div>
+                    <div className="item">Fats, g <span className="bolder jir"></span></div>
+                    <div className="item">Carbohydrates, g <span className="bolder uglev"></span></div>
                 </div>
                 <div className='polezn'>
                     <span className="ssylc">Useful links</span>

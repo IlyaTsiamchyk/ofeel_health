b061f3d85f6ac76ac8d25c00ea5583d3ee6702ca
Katsiaryna Valenchyts <k.valenchyts@codemaster.by>
2017-06-13 21:48:53 +0300

    Language of results changed on English.

--- diet_creator/static/js/calories.jsx
+++ diet_creator/static/js/calories.jsx
@@ -8,24 +8,23 @@ var Calories = React.createClass({
 		return (
 			<div>
 				<div className="calories-wrap">
-                    <div className="title">Разброс значений расчета калорий в день</div>
-                    <div className="item">Минимум <span className="bolder min">2506</span></div>
-                    <div className="item">Среднее <span className="bolder aver">2794</span><span className="bold proc"> Разброс: ±25.18% (704ккал)</span></div>
-                    <div className="item">Максимум <span className="bolder max">3498</span></div>
+                    <div className="title">Variance of calorie calculation values per day, Kcal</div>
+                    <div className="item">Minimum <span className="bolder min">2506</span></div>
+                    <div className="item">Average <span className="bolder aver">2794</span><span className="bold proc"> Scatter: ±25.18% (704 Kcal)</span></div>
+                    <div className="item">Maximum <span className="bolder max">3498</span></div>
                 </div>
 				<div className="bju">
-                    <div className="title">Рекомендуемое суточное количество</div>
-                    <div className="item">Белки, г <span className="bolder belc">79.52</span></div>
-                    <div className="item">в т.ч. животные белки, г <span className="bolder jivotbelc">43.73</span></div>
-                    <div className="item">Жиры, г <span className="bolder jir">92.70</span></div>
-                    <div className="item">Углеводы, г <span className="bolder uglev">409.16</span></div>
+                    <div className="title">The recommended daily amount</div>
+                    <div className="item">Proteins, g <span className="bolder belc">79.52</span></div>
+                    <div className="item">Including animal proteins, g <span className="bolder jivotbelc">43.73</span></div>
+                    <div className="item">Fats, g <span className="bolder jir">92.70</span></div>
+                    <div className="item">Carbohydrates, g <span className="bolder uglev">409.16</span></div>
                 </div>
                 <div className='polezn'>
-                    <span className="ssylc">Полезные ссылки</span>
-                    <a href="#" className="item">Подбор диеты для похудения</a>
-                    <a href="#" className="item">Продукты ускоряющие метаболизм</a>
-                    <a href="#" className="item">Питание для спортсменов</a>
-                    <a className="recept" href="http://edimka.ru/eda/">Рецепты</a>
+                    <span className="ssylc">Useful links</span>
+                    <a href="http://edimka.ru/text/diets/index" className="item">Selection of diet for weight loss</a>
+                    <a href="https://bodystrong.info/nutrition" className="item">Food for athletes</a>
+                    <a className="recept" href="http://edimka.ru/eda/">Recipes</a>
                 </div>
 			</div>
 		);
